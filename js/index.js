3e7f67dff45e4dbe435365bf33ece50157648360
Artem Habinskyi <a.habinskyi@gmail.com>
2019-05-10 16:46:23 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,17 +1,54 @@
-document.querySelector('.openweather').addEventListener('click', getWeather);
+// document.querySelector('.openweather').addEventListener('click', getWeather);
 
-const XHR_DONE = 4;
-const HTTP_OK = 200;
+// const XHR_DONE = 4;
+// const HTTP_OK = 200;
 
 
-function getWeather() {
-    const XML = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
+// function getWeather() {
+//     const XML = new XMLHttpRequest();
+//     xhr.onreadystatechange = function () {
         
-        if (xhr.readyState === XHR_DONE && xhr.status === HTTP_OK) {
-            document.querySelector('.weather').innerHTML = xhr.responesXML;
-        }
-    }
-    xhr.open('GET', 'weather.html', true);
-    xhr.send();
+//         if (xhr.readyState === XHR_DONE && xhr.status === HTTP_OK) {
+//             document.querySelector('.weather').innerHTML = xhr.responesXML;
+//         }
+//     }
+//     xhr.open('GET', 'weather.html', true);
+//     xhr.send();
+// }
+
+// document.querySelector('.fetch-weather').addEventListener('click', fetchWeather);
+
+// function fetchWeather() {
+//     fetch('client-data.html')
+//         .then( response => response.text() )
+//         .then( html => document.querySelector('.html-container').innerHTML = html );
+// }
+
+const name = document.querySelector('.contact-form input[type=text]');
+const email = document.querySelector('.contact-form input[type=email]');
+const phoneNum = document.querySelector('.contact-form input[type=text]');
+const message = document.querySelector('.contact-form textarea');
+
+document.querySelector('.save-btn')
+  .addEventListener('click', saveText);
+
+document.querySelector('.load-btn')
+  .addEventListener('click', loadText);
+
+function saveText(e) {
+  e.preventDefault();
+  localStorage['name'] = name.value;
+  localStorage['email'] = email.value;
+  localStorage['phoneNum'] = email.value;
+  localStorage['message'] = message.value;
 }
+
+function loadText(e) {
+  e.preventDefault();
+  name.value = localStorage['name'] || '';
+  email.value = localStorage['email'] || '';
+  email.value = localStorage['phoneNum'] || ;
+  message.value = localStorage['message'] || '';
+}
+
+
