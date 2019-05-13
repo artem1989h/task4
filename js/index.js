e4bec4845bb382cd533feb6d3ba62558e610db5b
Artem Habinskyi <a.habinskyi@gmail.com>
2019-05-13 14:12:20 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -23,6 +23,33 @@
 //         .then( response => response.text() )
 //         .then( html => document.querySelector('.html-container').innerHTML = html );
 // }
+// const forecastAPI = 'https://api.openweathermap.org/data/2.5/weather?id=702550';
+// const keyAPI = '6769bee229c73c93d1cea3e53ea41fe5';
+// fetch(forecastAPI + keyAPI)
+//     .then(response => response.json())
+//     .then(weatherdata => {
+//         const celsiusTemperature = (weatherdata.main.temp - 273.15).toFixed(2);
+//         document.getElementById('temp').innerHTML = `<p> ${(weatherdata.main.temp - 273.15).toFixed(2)} <sup>o</sup>C</p>`;
+//         document.getElementById('humidity').innerHTML = `<p> ${weatherdata.main.humidity} %</p>`;
+//         document.getElementById('pressure').innerHTML = `<p> ${weatherdata.main.pressure} мм рт.ст.</p>`;
+//         document.getElementById('clouds').innerHTML = `<p> ${weatherdata.clouds.all} %</p>`;
+//         document.getElementById('wind').innerHTML = `<p> ${weatherdata.wind.speed} м/с</p>`;
+//     });
+    
+const forecastAPI = 'https://api.openweathermap.org/data/2.5/weather?id=702550';
+const keyAPI = '&APPID=0746e9cef51e45cd40c66c3f06fcecb1';
+fetch(forecastAPI + keyAPI)
+    .then(response => response.json())
+    .then(weatherdata => {
+        const celsiusTemperature = (weatherdata.main.temp - 273.15).toFixed(2);
+        document.getElementById('temp').innerHTML = `<p> ${(weatherdata.main.temp - 273.15).toFixed(2)} <sup>o</sup>C</p>`;
+        document.getElementById('humidity').innerHTML = `<p> ${weatherdata.main.humidity} %</p>`;
+        document.getElementById('pressure').innerHTML = `<p> ${weatherdata.main.pressure} мм рт.ст.</p>`;
+        document.getElementById('clouds').innerHTML = `<p> ${weatherdata.clouds.all} % <img src="" id="wicon" src="" alt="Weather icon"></p>`;
+        document.getElementById('wicon').src = "http://openweathermap.org/img/w/" + weatherdata.weather[0].icon + ".png";
+        document.getElementById('wind').innerHTML = `<p> ${weatherdata.wind.speed} м/с</p>`;
+    });
+
 
 const name = document.querySelector('.contact-form input[type=text]');
 const email = document.querySelector('.contact-form input[type=email]');
@@ -47,7 +74,7 @@ function loadText(e) {
   e.preventDefault();
   name.value = localStorage['name'] || '';
   email.value = localStorage['email'] || '';
-  email.value = localStorage['phoneNum'] || ;
+  email.value = localStorage['phoneNum'] || '';
   message.value = localStorage['message'] || '';
 }
 
